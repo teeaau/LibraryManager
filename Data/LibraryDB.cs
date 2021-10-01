54a6fea53d69026e65f183c3f7d5d93e533f067e
teeaau <changtraiyeumau@gmail.com>
2021-10-01 16:33:12 +0700

    update

--- Data/LibraryDB.cs
+++ Data/LibraryDB.cs
@@ -6,11 +6,26 @@ namespace Data
 {
     public class LibraryDB : DbContext
     {
+        // Your context has been configured to use a 'LibraryDB' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'Data.LibraryDB' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'LibraryDB' 
+        // connection string in the application configuration file.
         public LibraryDB()
             : base("name=LibraryDB")
         {
         }
 
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
     }
 
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
 }
